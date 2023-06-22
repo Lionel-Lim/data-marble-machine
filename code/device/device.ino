3269f77671dd61eb63420a5ccc131cd76e980c19
DY Lim <dlagus6412@gmail.com>
2023-06-22 03:47:42 +0100

    added board initialising

--- code/device/device.ino
+++ code/device/device.ino
@@ -7,12 +7,17 @@
 #include <addons/TokenHelper.h>
 // Provide the RTDB payload printing info and other helper functions.
 #include <addons/RTDBHelper.h>
-// #include </Users/dylim/Library/Arduino15/packages/firebeetle32/hardware/esp32/0.1.1/libraries/SD/src/SD.h>
+#include <Adafruit_NeoPixel.h>
 
 #define MQTT_SERVER "mqtt.cetools.org"
 #define MQTT_TOPIC "UCL/OPS/107/EM/gosund/#"
 // Firebase database address
 #define DATABASE_URL "https://energycelab-default-rtdb.europe-west1.firebasedatabase.app"
+// Digital IO pin connected to the NeoPixel attached to the board.
+#define PIXEL_PIN 0
+
+// Declare a NeoPixel object
+Adafruit_NeoPixel singlePixel(1, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
 
 // Define the Firebase Data object
 FirebaseData fbdo;
@@ -27,10 +32,16 @@ PubSubClient client(espClient);
 unsigned long dataMillis = 0;
 int count = 0;
 int led = LED_BUILTIN;
+bool booting = false;
 
 void setup()
 {
     Serial.begin(115200);
+
+    //Initalise single NeoPixel
+    singlePixel.begin();
+    singlePixel.setBrightness(128); // Half brightness
+    singlePixel.show();
 
     // set LED(13) to be an output pin
     pinMode(led, OUTPUT);
@@ -79,12 +90,41 @@ void setup()
 
 void loop() {
   if (!client.connected()) {
+    // While connecting to MQTT, light up red color
+    singlePixel.setPixelColor(0, singlePixel.Color(255, 0, 0)); // Red color
+    singlePixel.setPixelColor(1, singlePixel.Color(255, 0, 0)); // Red color
+    singlePixel.show(); // Apply the color
     reconnect();
+  }
+  // Once MQTT is connected
+  else if (booting == false) {
+    booting = true;
+    // Light up green color
+    singlePixel.setPixelColor(0, singlePixel.Color(0, 255, 0)); // Green color
+    singlePixel.setPixelColor(1, singlePixel.Color(0, 255, 0)); // Green color
+    singlePixel.show(); // Apply the color
+    delay(2000); // Wait for 2 seconds
+
+    uint32_t startMillis = millis();
+    Serial.print("Booting Started at: ");
+    Serial.println(startMillis);
+    while (millis() - startMillis < 5) { // Run for 5 seconds
+      rainbow(5, singlePixel);
+    }
+
+    // Turn off LED
+    singlePixel.clear(); // Clear the color
+    singlePixel.show(); // Apply the color
+    Serial.println("Booting Finished.");
   }
   client.loop();
 }
 
+
 void callback(char* topic, byte* payload, unsigned int length) {
+    if (!booting) {
+    return;
+  }
   Serial.println("Message is:");
   char msg[length+1];
   memcpy (msg, payload, length);
@@ -121,19 +161,26 @@ void reconnect() {
   }
 }
 
-// void loop()
-// {
-//   if (millis() - dataMillis > 5000 && Firebase.ready())
-//   {
-//     dataMillis = millis();
-//     String path = "/UsersData/";
-//     path += auth.token.uid.c_str();
-//     path += "/test/data";
-
-//     FirebaseJson json;
-//     json.set("value", count++);
-//     json.set("timestamp", Firebase.getCurrentTime());
-
-//     Serial.printf("Push data... %s\n", Firebase.pushJSON(firebaseData, path, json) ? "ok" : firebaseData.errorReason().c_str());
-//   }
-// }
+// Rainbow cycle along whole neoPixel. Pass delay time (in ms) between frames.
+void rainbow(int wait, Adafruit_NeoPixel neoPixel) {
+  // Hue of first pixel runs 3 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 3*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 3*65536/256 = 768 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 3*65536; firstPixelHue += 256) {
+    for(int i=0; i<neoPixel.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (neoPixel.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / neoPixel.numPixels());
+      // neoPixel.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through neoPixel.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      neoPixel.setPixelColor(i, neoPixel.gamma32(neoPixel.ColorHSV(pixelHue)));
+    }
+    neoPixel.show(); // Update neoPixel with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
